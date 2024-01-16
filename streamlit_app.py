554e89f4f4ed65ce920915bfcbb04eb49346f557
Lakka-appdev4ever <156731715+Lakka-appdev4ever@users.noreply.github.com>
2024-01-16 13:33:47 +1100

    Update streamlit_app.py
    import files

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,5 +11,5 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
-my_fruit_list = pandas.read_csv("http://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.datframe(my_first_list)
+'my_fruit_list' = pandas.read_csv("http://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.datframe('my_first_list')
