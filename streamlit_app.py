2ae5082179a92dd5ecf7566a7af479f74dd56c1b
Lakka-appdev4ever <156731715+Lakka-appdev4ever@users.noreply.github.com>
2024-01-16 13:11:01 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,8 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.title('BreaKfast Menu')
 
-Omega 3 & Blueberry oatmeal
+'Omega 3 & Blueberry oatmeal'
 
-Kale, Spinach & Rocket smoothie
+'Kale, Spinach & Rocket smoothie'
 
-Hard-Boiled Free-Range egg
+'Hard-Boiled Free-Range egg'
